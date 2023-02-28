c2d7e754354ba39d2344558af52fe3c3b9740020
Emir Marat <emmarrat@gmail.com>
2023-02-28 17:52:49 +0600

    fix: navbar, added link for adding transaction

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppBar, Grid, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Toolbar, Typography} from "@mui/material";
 import {Link} from '../../styledComponents';
 
 
@@ -7,15 +7,17 @@ const Navbar = () => {
   return (
     <AppBar position="sticky" sx={{mb: 2}}>
       <Toolbar>
-        <Grid container>
-          <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-            <Link to="/">Finance Tracker</Link>
-          </Typography>
-
+        <Typography variant="h6" component="div" sx={{flexGrow: 4}}>
+          <Link to="/">Finance Tracker</Link>
+        </Typography>
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', justifyContent: 'space-evenly' } }}>
           <Typography variant="subtitle1" component="div">
             <Link to="/categories">Categories</Link>
           </Typography>
-        </Grid>
+          <Typography variant="subtitle1" component="div">
+            <Link to="/add-transaction">Add</Link>
+          </Typography>
+        </Box>
       </Toolbar>
     </AppBar>
   );
